00370f57d4abb32b6abd41035e2a4179dd5369dd
Juan-Altamirano <101400097+Juan-Altamirano@users.noreply.github.com>
2022-05-10 13:54:07 -0300

    Aceptar y mensaje por consola

---
+++ Assets/DataInputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataInputManager : MonoBehaviour
+{
+
+    public Text txtUsername;
+
+    public void ShowGreetings()
+    {
+        Debug.Log("Hola " + txtUsername.text);
+    }
+}
